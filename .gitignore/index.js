9418daff24fb28359ca6456e08431db26da8e41a
Bubbles10232 <57570896+Bubbles10232@users.noreply.github.com>
2020-07-06 11:49:25 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,7 @@ bot.on('ready', function() {
     console.log("Connectedç");
 });
 
-bot.login("NzEyNTgzMjk3NDEwMDA3MDcx.XwLrXw.jgFgeYESkrVSFlPAryQFGlZRwo0");
+bot.login(process.env.TOKEN);
 
 
 bot.on('message', message => {

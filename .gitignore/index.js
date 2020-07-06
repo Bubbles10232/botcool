82fd28043bf89c2c439e73abaea5bd2bc53b3005
Bubbles10232 <57570896+Bubbles10232@users.noreply.github.com>
2020-07-06 11:55:41 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-var prefix = ("*")
+var prefix = ("-")
 
 bot.on('ready', function() {
-    bot.user.setGame("Command: *help");
+    bot.user.setGame("Command: -help");
     console.log("Connectedç");
 });
 
@@ -13,7 +13,7 @@ bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n -*help");
+        message.channel.sendMessage("Liste des commandes: \n --help");
     }
 
     if (message.content === "Salut"){

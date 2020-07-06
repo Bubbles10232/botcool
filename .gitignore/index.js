534ac7b91368f4084285ed81120bd5e8e37638bf
Bubbles10232 <57570896+Bubbles10232@users.noreply.github.com>
2020-07-06 11:47:44 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,10 +5,10 @@ var prefix = ("*")
 
 bot.on('ready', function() {
     bot.user.setGame("Command: *help");
-    console.log("Connected");
+    console.log("Connectedç");
 });
 
-bot.login(process.env.TOKEN);
+bot.login("NzEyNTgzMjk3NDEwMDA3MDcx.XwLrXw.jgFgeYESkrVSFlPAryQFGlZRwo0");
 
 
 bot.on('message', message => {
@@ -20,4 +20,21 @@ bot.on('message', message => {
         message.reply("Bien le bonjour. :)");
         console.log("Commande Salut effectué");
     }
+
+    if (message.contebt === prefix + "fabriquant"){
+        message.reply("_Ce bot a été crée par Bubbles_");
+        console.log("Commande effectué");
+    }
+
+    if (message.content === prefix + "embed"){
+        var embed = new Discord.RichEmbed()
+        .setTitle("EMBED")
+        .setDescription("Ceci est un embed")
+        .addField(".help","Page d'aide", true)
+        .addField("Embed01","Embed01 ! :) Suivez les tuto de [Bubbles](https://www.youtube.com/c/pzhcodage", true)
+        .setColor("0xFF8000")
+        .SetFooter("Bon moment parmis nous ! :)")
+    message.channel.sendEmbed(embed);
+    }
+        
 });
